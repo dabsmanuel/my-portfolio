c8b395fc8bcf69a8a06caad0c30cf9d3a91f2c3c
Emmanuel Oyibo <75739167+dabsmanuel@users.noreply.github.com>
2022-08-29 16:16:36 +0100

    updated resume

--- src/Components/Header/Button.jsx
+++ src/Components/Header/Button.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import resume from '../../Assets/Resume.pdf'
 
 const Button = () => {
   return (
     <div className='cta'>
-        <a href='https://docs.google.com/document/d/14GranEqpJpdSH1wqI3BPaGzbESzsKdsh7LNbSvGC-zQ/edit?usp=sharing' target='_blanc' className='btn'>Download CV</a>
+        <a href={resume} target='_blanc' className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
