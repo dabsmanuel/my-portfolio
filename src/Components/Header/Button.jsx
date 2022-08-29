2851872a5f92bf0665e42d767c451768a788adfe
Emmanuel Oyibo <75739167+dabsmanuel@users.noreply.github.com>
2022-08-29 15:52:34 +0100

    updated resume

--- src/Components/Header/Button.jsx
+++ src/Components/Header/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = () => {
   return (
     <div className='cta'>
-        <a href='https://docs.google.com/document/d/12oD24p6TvZfDdlx-IAiXy2I0PlDYtgED2OSI9lA_fWc/edit?usp=sharing' className='btn'>Download CV</a>
+        <a href='https://docs.google.com/document/d/14GranEqpJpdSH1wqI3BPaGzbESzsKdsh7LNbSvGC-zQ/edit?usp=sharing' target='_blanc' className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
