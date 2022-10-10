47a54e6b9f606f7221d296b7b4e8e3dbb88dd6cd
Emmanuel Oyibo <75739167+dabsmanuel@users.noreply.github.com>
2022-10-10 03:24:10 +0100

    updated portfolio

--- src/Components/Portfolio/Portfoliodata.js
+++ src/Components/Portfolio/Portfoliodata.js
@@ -1,54 +1,56 @@
 import './Portfolio.css';
 import furniture from './../../Assets/furniture.png'
 import equipafrica from './../../Assets/equipafrica.png'
-import suites from "./../../Assets/suite.png";
 import elis from "./../../Assets/elis-regina.png"
 import portimage from "./../../Assets/portfolio.png";
-import EquipList from '../../Assets/Screenshot_8.png'
+import zuri from "../../Assets/zuri.png"
+import reasons from "../../Assets/28-reasons-why.png"
 
 
 const data = [
   {
     id: 1,
-    image: portimage,
-    title: "My Portfolio",
-    github: "https://github.com/dabsmanuel/official_portfolio",
-    demo: "https://dabsmanuel.netlify.app",
-  },
-  {
-    id: 2,
     image: equipafrica,
     title: "Equip Africa",
-    github: "https://github.com/barnyvic/equipAfrica",
-    demo: "https://equip-africa.vercel.app",
+    github: "https://github.com/dabsmanuel/EquipAfrica",
+    demo: "https://equipafrica.netlify.app",
   },
+
+  {
+    id: 2,
+    image: reasons,
+    title: "28 reasons why",
+    github: "https://github.com/dabsmanuel/28-reasons-why-react-app",
+    demo: "https://for-sharon.netlify.app",
+  },
+
   {
     id: 3,
-    image: suites,
-    title: "Suites Hotel Design from Figma",
-    github: "https://github.com/dabsmanuel/hotel-suites-react-app",
-    demo: "https://suiteshotel.netlify.app",
+    image: zuri,
+    title: "A zuri frontend project",
+    github: "https://github.com/Barnyvic/Zuri-frontend",
+    demo: "https://zuri-frontend.vercel.app",
   },
   {
     id: 4,
+    image: portimage,
+    title: "My Portfolio",
+    github: "https://github.com/dabsmanuel/my-portfolio",
+    demo: "https://dabsmanuel.netlify.app",
+  },
+  {
+    id: 5,
     image: furniture,
     title: "A furniture Website from Figma",
     github: "https://github.com/dabsmanuel/furniture-web",
     demo: "https://furnishop-web.netlify.app",
   },
   {
-    id: 5,
+    id: 6,
     image: elis,
     title: "Tribute Page To Elis Regina",
     github: "https://github.com/dabsmanuel/tribute-react",
     demo: "https://elis-regina.netlify.app",
-  },
-  {
-    id: 6,
-    image: EquipList,
-    title: "Equip Africa Waitlist",
-    github: "https://github.com/dabsmanuel/EquipAfrica-wait-list",
-    demo: "https://equipafrica-waitlist.netlify.app/",
   },
 ];
 
